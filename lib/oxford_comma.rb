db4a2338c45e22b029eae0db80e53eeb7a0133c9
ryan.bollettino@gmail.com <bollo@bollo-laptop.localdomain>
2020-11-16 21:40:04 -0600

    commit tree 86efbf19f66aa4b9428e96f033aa9457248d1dca

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+    
+        if array.length == 2
+              array.join(" and ")
+        elsif array.length > 2
+            two_plus = array[0...-1]
+            two_string = two_plus.join(", ")
+            two_string << ", and "
+            two_string << array[-1]
+            
+        else
+              array.join
+        end
 end
